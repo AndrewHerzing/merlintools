83de0382c173d88f1bcba9255c4eee6154f23e02
Herzing <aherzing@NIST.GOV>
2020-10-30 17:38:04 -0400

    Added tests for parsing functions

--- merlintools/tests/test_io.py
+++ merlintools/tests/test_io.py
@@ -1,12 +1,12 @@
 import merlintools
 import os
+import numpy as np
 from pyxem.signals.electron_diffraction2d import ElectronDiffraction2D
 
 merlin_path = os.path.dirname(merlintools.__file__)
 
 
-class TestMIB:
-
+class TestReadMIB:
     def test_load_mib_with_dm(self):
         dmfilename = os.path.join(merlin_path, "tests",
                                   "test_data", "HAADF.dm3")
@@ -67,3 +67,21 @@ class TestMIB:
         assert type(s) is ElectronDiffraction2D
         assert s.axes_manager.signal_shape == (256, 256)
         assert s.axes_manager.navigation_shape == (20, 20)
+
+    class TestHeaderParsing:
+
+        def test_hdr_parser(self):
+            hdrfile = os.path.join(merlin_path, "tests", "test_data", "merlin.hdr")
+            header = merlintools.io.parse_hdr(hdrfile)
+            assert type(header) is dict
+            assert 'TimeStamp' in header.keys()
+            assert 'SoftwareVersion' in header.keys()
+            assert np.int(header['TotalFrames']) == 11007
+        
+        def test_mib_parser(self):
+            mibfile = os.path.join(merlin_path, "tests", "test_data", "merlin.mib")
+            header = merlintools.io.parse_mib_header(mibfile)
+            assert type(header) is dict
+            assert 'HeaderID' in header.keys()
+            assert 'ExtCounterDepth' in header.keys()
+            assert header['DataOffset'] == 384
