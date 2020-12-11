ac5ca6ba72472bfa0f539a164d8222ec18b31aa5
Herzing <aherzing@NIST.GOV>
2020-12-11 18:16:59 -0500

    Improved preprocessing module

--- merlintools/preprocessing.py
+++ merlintools/preprocessing.py
@@ -5,7 +5,7 @@ import os
 
 def preprocess(datapath="./", mibfile=None, dmfile=None, com_threshold=3,
                r_bf=15, r_adf_inner=20, r_adf_outer=65, save_results=True,
-               return_all=False):
+               return_all=False, overwrite=False):
     if not mibfile:
         mibfile = glob.glob(datapath + "*.mib")[0]
     if not dmfile:
@@ -28,20 +28,35 @@ def preprocess(datapath="./", mibfile=None, dmfile=None, com_threshold=3,
     sum_pattern.compute()
 
     bf = s.virtual_bright_field(cx=128, cy=128, r=r_bf)
-    adf = s.virtual_annular_dark_field(128, 128,
-                                       r_inner=r_adf_inner, r=r_adf_outer)
+    adf = s.virtual_annular_dark_field(128, 128, r_inner=r_adf_inner,
+                                       r=r_adf_outer)
 
     if save_results:
-        s.save(os.path.splitext(mibfile)[0] + ".hspy")
+        s.save(os.path.splitext(mibfile)[0] + ".hspy", overwrite=overwrite)
 
-        sum_pattern.save(os.path.splitext(mibfile)[0] + "_SumPattern.hspy")
-        sum_pattern.save(os.path.splitext(mibfile)[0] + "_SumPattern.tiff")
+        sum_pattern_out = sum_pattern.deepcopy()
+        sum_pattern_out.data = (255*sum_pattern_out.data /
+                                sum_pattern_out.data.max())
+        sum_pattern_out.change_dtype('uint8')
+        sum_pattern.save(os.path.splitext(mibfile)[0] + "_SumPattern.hspy",
+                         overwrite=overwrite)
+        sum_pattern_out.save(os.path.splitext(mibfile)[0] + "_SumPattern.png",
+                             overwrite=overwrite)
 
-        bf.save(os.path.splitext(mibfile)[0] + "_BF.hspy")
-        bf.save(os.path.splitext(mibfile)[0] + "_BF.tiff")
+        bf_out = bf.deepcopy()
+        bf_out.data = 255*bf_out.data/bf_out.data.max()
+        bf_out.change_dtype('uint8')
+        bf.save(os.path.splitext(mibfile)[0] + "_BF.hspy", overwrite=overwrite)
+        bf_out.save(os.path.splitext(mibfile)[0] + "_BF.png",
+                    overwrite=overwrite)
 
-        adf.save(os.path.splitext(mibfile)[0] + "_ADF.hspy")
-        adf.save(os.path.splitext(mibfile)[0] + "_ADF.tiff")
+        adf_out = adf.deepcopy()
+        adf_out.data = 255*adf_out.data/adf_out.data.max()
+        adf_out.change_dtype('uint8')
+        adf_out.save(os.path.splitext(mibfile)[0] + "_ADF.hspy",
+                     overwrite=overwrite)
+        adf_out.save(os.path.splitext(mibfile)[0] + "_ADF.png",
+                     overwrite=overwrite)
 
     if return_all:
         return s, sum_pattern, bf, adf
