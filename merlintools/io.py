7a14dcb4c6c02c58a110b1b7985e8d907fe28ec7
Andrew Herzing <andrew.herzing@nist.gov>
2022-03-08 16:10:02 -0500

    Changed output of apertures in io.save_results

--- merlintools/io.py
+++ merlintools/io.py
@@ -593,15 +593,8 @@ def save_results(h5filename, dataset):
                 grp = h5.create_group('images')
                 for im in dataset['images'].keys():
                     grp.create_dataset(im, data=dataset['images'][im])
-            elif k == 'apertures':
-                grp = h5.create_group('apertures')
-                if type(dataset['apertures']) is np.ndarray:
-                    for apt in dataset['apertures'].keys():
-                        grp.create_dataset(apt, data=dataset['apertures'][apt])
-            elif k == 'radii':
-                grp = h5.create_group('radii')
-                if dataset['radii']:
-                    pass
+            elif k == 'apertures' and type(dataset['apertures']) is np.ndarray:
+                h5.create_dataset('apertures', data=dataset['apertures'])
             else:
                 h5.create_dataset(k, data=dataset[k])
     return
