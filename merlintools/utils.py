cea20ff63f1a0b3f9da80b9faec7a47f287f6127
Andrew Herzing <andrew.herzing@nist.gov>
2025-04-15 10:11:56 -0400

    Added calc_euler function to calibration.py

--- merlintools/utils.py
+++ merlintools/utils.py
@@ -18,6 +18,8 @@ import glob
 from scipy.interpolate import interp1d
 import matplotlib.pylab as plt
 import fpd.fpd_file as fpdf
+from scipy.spatial.transform import Rotation
+
 try:
     import py4DSTEM
 except ImportError:
@@ -30,12 +32,12 @@ merlintools_path = os.path.dirname(merlintools.__file__)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-e = 1.602e-19       # Charge of electron (Coulombs)
-m0 = 9.109e-31      # Rest mass of electron (kg)
-m0c2 = 511          # Rest energy of electron (keV)
-h = 6.626e-34       # Planck's constant
-c = 2.998e8         # Speed of light in vacuum (m/s)
-Na = 6.0221409e23   # Avogadro's number
+e = 1.602e-19  # Charge of electron (Coulombs)
+m0 = 9.109e-31  # Rest mass of electron (kg)
+m0c2 = 511  # Rest energy of electron (keV)
+h = 6.626e-34  # Planck's constant
+c = 2.998e8  # Speed of light in vacuum (m/s)
+Na = 6.0221409e23  # Avogadro's number
 
 
 def get_lattice_spacings(material):
@@ -53,20 +55,17 @@ def get_lattice_spacings(material):
     d : float
         Lattice spacing in nanometers
     """
-    if material.lower() == 'aupd':
-        hkls = {'111': 2.31, '200': 2.00, '220': 1.41,
-                '311': 1.21, '222': 1.15}
+    if material.lower() == "aupd":
+        hkls = {"111": 2.31, "200": 2.00, "220": 1.41, "311": 1.21, "222": 1.15}
         return hkls
-    elif material.lower() == 'au':
-        hkls = {'111': 2.36, '200': 2.04, '220': 1.44,
-                '311': 1.23, '222': 1.18}
+    elif material.lower() == "au":
+        hkls = {"111": 2.36, "200": 2.04, "220": 1.44, "311": 1.23, "222": 1.18}
         return hkls
-    elif material.lower() == 'si':
-        hkls = {'111': 3.14, '200': 2.72, '220': 1.92,
-                '311': 1.64, '222': 1.57}
+    elif material.lower() == "si":
+        hkls = {"111": 3.14, "200": 2.72, "220": 1.92, "311": 1.64, "222": 1.57}
         return hkls
-    elif material.lower() == 'asbestos':
-        hkls = {'020': 8.98, '011': 4.87}
+    elif material.lower() == "asbestos":
+        hkls = {"020": 8.98, "011": 4.87}
     else:
         raise ValueError("Unknown material.  Must be 'Au-Pd', 'Au', or 'Si'")
     return hkls
@@ -195,7 +194,7 @@ def voltage_to_wavelength(voltage, relativistic=False):
         Calculated wavelength (in nanometers)
     """
     if relativistic:
-        correction = (1 + ((e * voltage * 1000) / (2 * m0 * c**2)))
+        correction = 1 + ((e * voltage * 1000) / (2 * m0 * c**2))
         wavelength = h / np.sqrt(2 * m0 * e * voltage * 1000 * correction)
     else:
         wavelength = h / np.sqrt(2 * m0 * e * voltage * 1000)
@@ -264,8 +263,9 @@ def calc_dose_probe(probe_current, probe_fwhm, dwell_time):
 
     """
     n_electrons = 6.242e18 * probe_current * dwell_time
-    dose = n_electrons / (np.pi * (probe_fwhm / 2)**2)
+    dose = n_electrons / (np.pi * (probe_fwhm / 2) ** 2)
     return dose
+
 
 def calc_dose_limited_resolution(probe_current, dwell_time, dose):
     """
@@ -289,6 +289,7 @@ def calc_dose_limited_resolution(probe_current, dwell_time, dose):
     r = np.sqrt(probe_current * 6.24e18 * dwell_time / np.pi / dose)
     return r
 
+
 def _fit_func(x, A, exp):
     return A * x**exp
 
@@ -301,23 +302,26 @@ def _extrapolate_calibration(cl, calibrations):
 
 
 # Calibrations in mrads/pixel
-calibration_file_80kV = os.path.join(merlintools_path, "calibrations",
-                                     "80kV_calibrations.json")
-with open(calibration_file_80kV, 'r') as fp:
+calibration_file_80kV = os.path.join(
+    merlintools_path, "calibrations", "80kV_calibrations.json"
+)
+with open(calibration_file_80kV, "r") as fp:
     cal_80kV = json.load(fp)
 
-calibration_file_200kV = os.path.join(merlintools_path, "calibrations",
-                                      "200kV_calibrations.json")
-with open(calibration_file_200kV, 'r') as fp:
+calibration_file_200kV = os.path.join(
+    merlintools_path, "calibrations", "200kV_calibrations.json"
+)
+with open(calibration_file_200kV, "r") as fp:
     cal_200kV = json.load(fp)
 
-calibration_file_300kV = os.path.join(merlintools_path, "calibrations",
-                                      "300kV_calibrations.json")
-with open(calibration_file_300kV, 'r') as fp:
+calibration_file_300kV = os.path.join(
+    merlintools_path, "calibrations", "300kV_calibrations.json"
+)
+with open(calibration_file_300kV, "r") as fp:
     cal_300kV = json.load(fp)
 
 
-def get_calibration(beam_energy, cl, units='mrads'):
+def get_calibration(beam_energy, cl, units="mrads"):
     """
     Return reciprocal space calibration given the beam energy and camera length.
 
@@ -350,28 +354,31 @@ def get_calibration(beam_energy, cl, units='mrads'):
     elif beam_energy == 300:
         calibration_dictionary = cal_300kV
     else:
-        raise ValueError("No calibration for beam energy: %s. "
-                         "Must be 80, 200, or 300." % str(beam_energy))
+        raise ValueError(
+            "No calibration for beam energy: %s. "
+            "Must be 80, 200, or 300." % str(beam_energy)
+        )
     if str(cl) in calibration_dictionary.keys():
         calibration = calibration_dictionary[str(cl)]
         logger.info("Camera length found in calibration table.")
     else:
-        calibration =\
-            np.round(_extrapolate_calibration(cl, calibration_dictionary), 3)
-        logger.info("Camera length not in calibration table. "
-                    "Calibration will be extrapolated.")
+        calibration = np.round(_extrapolate_calibration(cl, calibration_dictionary), 3)
+        logger.info(
+            "Camera length not in calibration table. Calibration will be extrapolated."
+        )
 
-    if units == 'mrads':
+    if units == "mrads":
         pass
-    elif units == 'q':
+    elif units == "q":
         wavelength = 10 * voltage_to_wavelength(beam_energy, True)
         calibration = (4 * np.pi * np.sin(calibration / 2000)) / wavelength
-    elif units == 'k':
+    elif units == "k":
         wavelength = 10 * voltage_to_wavelength(beam_energy, True)
         calibration = (2 * np.sin(calibration / 2000)) / wavelength
     else:
-        raise ValueError("Units (%s) not understood. "
-                         "Must be 'mrads', 'q', or 'k'" % units)
+        raise ValueError(
+            "Units (%s) not understood. Must be 'mrads', 'q', or 'k'" % units
+        )
     return calibration
 
 
@@ -389,13 +396,29 @@ def optimize_parameters(beam_energy, min_q, max_q):
         Maximum scattering angle of interest in q units (A^-1/pixel)
 
     """
-    cls = ['38', '48', '60', '77', '100', '130', '160', '195', '245', '300',
-           '380', '450', '600', '770', '900', '1200']
+    cls = [
+        "38",
+        "48",
+        "60",
+        "77",
+        "100",
+        "130",
+        "160",
+        "195",
+        "245",
+        "300",
+        "380",
+        "450",
+        "600",
+        "770",
+        "900",
+        "1200",
+    ]
 
     cals = np.zeros(len(cls))
     q_256 = np.zeros(len(cls))
     for i in range(0, len(cls)):
-        cals[i] = get_calibration(beam_energy, cls[i], units='q')
+        cals[i] = get_calibration(beam_energy, cls[i], units="q")
         q_256[i] = cals[i] * 256
 
     max_cl = cls[np.where(q_256 > 1.5 * max_q)[0][-1]]
@@ -412,18 +435,20 @@ def optimize_parameters(beam_energy, min_q, max_q):
 
     print("Suggested ADF Mask Locations")
     print("############################\n")
-    print("CL (mm)\t\tLocation for q_min (pixels))\t\tLocation for q_max"
-          "(pixels)")
-    print("-----------------------------------------------------------------"
-          "---------------------")
+    print("CL (mm)\t\tLocation for q_min (pixels))\t\tLocation for q_max(pixels)")
+    print(
+        "-----------------------------------------------------------------"
+        "---------------------"
+    )
     for i in range(0, len(cals)):
-        print("%s\t\t\t%.1f\t\t\t\t\t%.1f" %
-              (cls[i], min_q / cals[i], max_q / cals[i]))
+        print("%s\t\t\t%.1f\t\t\t\t\t%.1f" % (cls[i], min_q / cals[i], max_q / cals[i]))
     return
 
 
 def load_calibration_file(beam_energy, camera_length, also_alpha_data=False):
-    datapath = os.path.join(merlintools_path, "calibrations", "data", "%skV" % beam_energy)
+    datapath = os.path.join(
+        merlintools_path, "calibrations", "data", "%skV" % beam_energy
+    )
     datafile = "CL_%smm.mib" % camera_length
     dp = merlintools.io.read_single_mib(datapath + "/" + datafile, [256, 256])
     if also_alpha_data:
@@ -434,7 +459,9 @@ def load_calibration_file(beam_energy, camera_length, also_alpha_data=False):
         return dp
 
 
-def get_2d_scattering_profile(Z, composition, q_range=[0, 2], q_size=256, plot_result=False, figsize=(9, 4)):
+def get_2d_scattering_profile(
+    Z, composition, q_range=[0, 2], q_size=256, plot_result=False, figsize=(9, 4)
+):
     """
     Calculate a 2D scattering profile.
 
@@ -454,6 +481,7 @@ def get_2d_scattering_profile(Z, composition, q_range=[0, 2], q_size=256, plot_r
         Size of figure if result is to be plotted
 
     """
+
     def _interpolation_function(d, y, n):
         x = np.arange(n)
         f = interp1d(x, y)
@@ -467,27 +495,52 @@ def get_2d_scattering_profile(Z, composition, q_range=[0, 2], q_size=256, plot_r
     q = np.linspace(q_range[0], q_range[1], q_size)
 
     if haspy4dstem:
-        atom_sf = py4DSTEM.process.utils.single_atom_scatter(Z, composition, q, 'A')
-        atom_sf.get_scattering_factor(Z, composition, q, 'A')
+        atom_sf = py4DSTEM.process.utils.single_atom_scatter(Z, composition, q, "A")
+        atom_sf.get_scattering_factor(Z, composition, q, "A")
     else:
-        raise ImportError('py4DSTEM not installed.  This function is not available.')
+        raise ImportError("py4DSTEM not installed.  This function is not available.")
 
     x, y = np.meshgrid(range(q_size), range(q_size))
-    d = np.sqrt((x - (q_size / 2) + 1)**2 + (y - (q_size / 2) + 1)**2)
+    d = np.sqrt((x - (q_size / 2) + 1) ** 2 + (y - (q_size / 2) + 1) ** 2)
     sp = _interpolation_function(d, atom_sf.fe, q_size)
 
     if plot_result:
         fig, ax = plt.subplots(1, 2, figsize=figsize)
-        ax[0].plot(q, atom_sf.fe, 'r-')
-        ax[0].set_xlabel('Scattering Vector [1/Angstrom]', size=12)
-        ax[0].set_ylabel('Single Atom Scattering', size=12)
+        ax[0].plot(q, atom_sf.fe, "r-")
+        ax[0].set_xlabel("Scattering Vector [1/Angstrom]", size=12)
+        ax[0].set_ylabel("Single Atom Scattering", size=12)
         ax[0].set_xlim([0, 2])
-        ax[1].imshow(sp, cmap='inferno')
+        ax[1].imshow(sp, cmap="inferno")
         plt.tight_layout()
     return sp
 
 
 def get_simulated_data():
-    datafile = os.path.join(merlintools_path, "tests", "test_data", "SimulatedData.hdf5")
+    datafile = os.path.join(
+        merlintools_path, "tests", "test_data", "SimulatedData.hdf5"
+    )
     data = fpdf.fpd_to_tuple(datafile, fpd_check=False)
     return data
+
+
+def calc_euler(uvw):
+    """
+    Calculate crystal direction rotation.
+
+    Calculates the Euler rotation angles in ZXZ notation need to to align a given crystal direction [uvw]
+    with the incident beam direction [001].
+
+    Parameters
+        uvw : list or numpy.ndarray
+            Miller indices for the crystal direction to be rotated
+
+    Returns
+        euler_zxz : numpy.ndarray
+            Euler angles for rotation
+    """
+    beam_direction = np.array([0, 0, 1])
+    uvw = uvw / np.linalg.norm(uvw)
+    rotation = Rotation.align_vectors(beam_direction[np.newaxis, :], uvw[np.newaxis, :])
+    euler_zxz = rotation[0].as_euler("zxz")
+    euler_zxz = np.degrees(euler_zxz)
+    return euler_zxz
